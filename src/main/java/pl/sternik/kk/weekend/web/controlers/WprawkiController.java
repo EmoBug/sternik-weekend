ffb2d373886261d234ce3fe2f4065ae0986b27b0
Home <Home@asus>
2017-04-23 14:11:44 +0200

    nie4

--- src/main/java/pl/sternik/kk/weekend/web/controlers/WprawkiController.java
+++ src/main/java/pl/sternik/kk/weekend/web/controlers/WprawkiController.java
@@ -2,15 +2,34 @@ package pl.sternik.kk.weekend.web.controlers;
 
 import java.util.Date;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import pl.sternik.kk.weekend.entities.Moneta;
+import pl.sternik.kk.weekend.entities.Status;
+import pl.sternik.kk.weekend.repositories.MonetaAlreadyExistsException;
+import pl.sternik.kk.weekend.repositories.MonetyRepository;
+import pl.sternik.kk.weekend.repositories.NoSuchMonetaException;
 
 @Controller
 public class WprawkiController {
+	@Autowired
+	@Qualifier("tablica")
+	MonetyRepository baza;
 
-    @RequestMapping("/wprawki")
+    @RequestMapping(path="/wprawki", method=RequestMethod.GET)
     public String wprawki(ModelMap model) {
         model.put("msg", "Wartosc z modelu");
         model.addAttribute("data", new Date());
@@ -24,5 +43,50 @@ public class WprawkiController {
         model.addAttribute("data", new Date());
         return "wprawki";
     }
+    
+    @GetMapping("/wprawki2")
+    @ResponseBody
+    public String wprawkiParam(@RequestParam("cos") String cosParam, ModelMap model)
+    		{
+    	return "wprawki z param cos=" + cosParam;
+    		}
 
+    @GetMapping("/wprawki3")
+    @ResponseBody
+    public String wprawkiHeader(@RequestHeader("User-Agent") String cosParam, ModelMap model)
+    {
+    	return "Używasz przeglądarki: "+cosParam;
+    }
+    
+    @RequestMapping(value = "wprawki/monety/{id}/json", produces = "application/json")
+    @ResponseBody
+    public ResponseEntity<Moneta> viewAsJson(@PathVariable("id") Long id, final ModelMap model)
+    {
+    	Moneta m;
+    	try {
+    		m=baza.readById(id);
+    		return new ResponseEntity<Moneta>(m, HttpStatus.OK);
+    	}
+    	catch (NoSuchMonetaException e)
+    	{
+    		System.out.println(e.getClass().getName());
+    		m = new Moneta();
+    		m.setNumerKatalogowy(id);
+    		m.setKrajPochodzenia("Polska");
+    		m.setStatus(Status.NOWA);
+    		m.setNominal(10L);
+    	}
+    	try
+    	{
+    		baza.create(m);
+    	}
+    	catch(MonetaAlreadyExistsException el)
+    	{
+    		System.out.println(el.getClass().getName());
+    	}
+    	return new ResponseEntity<Moneta>(m, HttpStatus.CREATED);
+    	}
+    
 }
+
+
